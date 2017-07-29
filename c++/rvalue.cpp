f8ded2297bd5fe664124be8c491ba282e8503445
Bo Dong <bo_dong@apple.com>
2017-07-29 22:57:26 +0800

    correct pass by reference

--- c++/rvalue.cpp
+++ c++/rvalue.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
-void swap(int x&, int y&){
+void swap(int& x, int &y, int & z){
 	int temp = x;
 	x=y;
 	y=temp;
 }
 
 int main(){
-	int x=10, y=0;
-	swap(x,y);
-	std::cout << "x: " <<x <<" y: " <<y <std::<endl;
+	int x=10, y=0, z=100;
+	swap(x,y, z);
+	std::cout << "x: " <<x <<" y: " <<y <<std::endl;
 	
 	return 0;
 }
+
+// compile gcc rvalue.cpp
