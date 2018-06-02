6e6caecd6665cac27dbc5fe3944ddecba143b4b2
bo_dong <bo_dong@apple.com>
2018-06-02 09:23:59 +0800

    deep dive interview exam

--- c++/interview/rvalue.cpp
+++ c++/interview/rvalue.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <string>
 using namespace std;
+
+//g++ rvalue.cpp -std=c++11 -o out/rvalue 
+//rvalue is C++11 feature
+
 int g_id=0;
 struct Person  
 {
@@ -15,6 +19,8 @@ struct Person
 		id = g_id++;
 		cout << "2000 p.name " <<p.name << " p.id " <<p.id << " my name " <<name <<" id " << id << "\n";
 	}
+
+	// this is copy constructor, without this, vector.push_back(a_person) will fail to be compiled
 	Person(const Person& p):name(move(p.name)), age(p.age){
 		id = g_id++;
 		cout << "2500 p.name " <<p.name << " p.id " <<p.id << " my name " <<name <<" id " << id << "\n";		
@@ -26,15 +32,20 @@ struct Person
 
 int main()
 {
-	string a= "Zhang San";
-	Person pa = Person(a, 20);
-	cout<<"1 : "<<a <<endl;
 	vector<Person> vp;
-	vp.push_back(pa);
-	cout<<"2 : "<<a <<endl;
-	a = "Li Si";
-	vp.push_back(Person(a, 21));
-	cout<<"3 : \n";
-	vp.emplace_back("Zhao 5",22);
+
+	cout<<"1 : \n"<<endl;		
+	Person p1 = Person("A1", 21);	
+	Person p2("A2",22 )
+	// vp.push_back(pa);
+
+	// cout<<"2 : \n"<<endl;
+	// String a = "A2";	 
+	// vp.push_back(Person(a, 21));
+	// cout<< a <<endl; //test move
+
+	// cout<<"3 : \n";
+	// vp.emplace_back("A3",22);
+
 	cout<<"4 : \n" ;
 }
