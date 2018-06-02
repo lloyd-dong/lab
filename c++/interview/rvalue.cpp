2a10268ec2f51ff08b30abc7f0b10a32d41ca373
bo_dong <bo_dong@apple.com>
2018-06-02 20:54:26 +0800

    test constructor

--- c++/interview/rvalue.cpp
+++ c++/interview/rvalue.cpp
@@ -12,8 +12,19 @@ struct Person
 	string name;
 	int age;
 	int id;
+	Person (){
+		cout <<"0000 default constructor" <<endl;
+	}
+	Person & operator=( const Person &rhs ){
+		if (this == &rhs)
+			return *this;
+		cout << "1111 operator =" <<endl; 
+		this->age = rhs.age;
+		this->id  = rhs.id;
+		return *this;
+	}
 	Person(string p_name, int p_age):name(move(p_name)), age(p_age),id(g_id++){
-		cout << "1000 p.name " <<p_name << " my name " <<name << " id " << id << "\n";
+		cout << "1000 p.name " <<p_name << " my name " <<name << " id " << id << endl;
 	}
 	Person(Person&& p):name(move(p.name)), age(p.age){
 		id = g_id++;
@@ -34,17 +45,19 @@ int main()
 {
 	vector<Person> vp;
 
-	cout<<"1 : \n"<<endl;		
+	cout << "1 : " << endl;		
 	Person p1 = Person("A1", 21);	
-	Person p2("A2",22 )
+	Person p10("A10",210);
+	Person p2("A2",22 );
+	Person p3 = p2;
 	// vp.push_back(pa);
 
-	// cout<<"2 : \n"<<endl;
+	// cout <<"2 : "<<endl;
 	// String a = "A2";	 
 	// vp.push_back(Person(a, 21));
-	// cout<< a <<endl; //test move
+	// cout << a << endl; //test move
 
-	// cout<<"3 : \n";
+	// cout <<"3 : " <<endl;
 	// vp.emplace_back("A3",22);
 
 	cout<<"4 : \n" ;
